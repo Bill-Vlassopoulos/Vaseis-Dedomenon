9b43fc88d892df0cb68b95c07b4d9701a03f7e27
Nektaria Zevgoula <152332336+Zevgoula@users.noreply.github.com>
2024-01-08 17:22:59 +0200

    query.py
    ante kai gamisou

--- query.py
+++ query.py
@@ -6,21 +6,12 @@ conn = sqlite3.connect("SmartRestaurant.db")
 cursor = conn.cursor()
 
 
-# return kratisis for a given day
-# Sto gui make sure days form 1-9 are given in 01-09 form
-def kratisi_for_day(daytime):
-    mylist = []
-    query = "SELECT id_kratisis, imera_ora FROM KRATISI"
+def print_all_from_pinaka(name):
+    query = "SELECT * FROM " + name
     cursor.execute(query)
-    results = cursor.fetchall()
-    for row in results:
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
-        if day == int((row[1])[8:10]):
-            mylist.append(row)
-    counter = len(mylist)
-    conn.close()
-
-    return counter, list  # returns number of kratisis + the tuple of kratisis for a given day
 
 
 def tuple_to_list(list):
@@ -32,37 +23,94 @@ def check_user(myusername, mypassword):
     query = "SELECT username, password FROM PELATIS"
     cursor.execute(query)
     results = cursor.fetchall()
-    pass
-    # check if input parameters match results
+    if (myusername, mypassword) in results:
+        return 1
+    else:
+        return 0
 
 
 # insert new customer
-def insert_user(name, lastname, phone, mail):
-    query = "INSERT INTO PELATIS (onoma, eponimo, tilefono, email) VALUES(?,?,?,?)"
-    cursor.execute(query, (name, lastname, phone, mail))
+def insert_user(name, lastname, phone, mail, username=None, password=None):
+    query = "INSERT INTO PELATIS (onoma, eponimo, tilefono, email, username, password) VALUES(?,?,?,?,?,?)"
+    cursor.execute(query, (name, lastname, phone, mail, username, password))
     conn.commit()
 
 
-def print_all_from_pinaka(name):
-    query = "SELECT * FROM " + name
+# inserts kritiki to database
+def insert_kritiki(bathmologia, perigrafi, id_pelati):
+    try:
+        query = "INSERT INTO KRITIKI (id, bathmologia, perigrafi, imerominia, id_pelati) VALUES(NULL,?,?,?,?)"
+        cursor.execute(query, (bathmologia, perigrafi, getdatetime(), id_pelati))
+        conn.commit()
+    except Exception as e:
+        print("Error " + str(e))
+
+
+# return kratisis for a given day
+# Sto gui make sure days form 1-9 are given in 01-09 form
+def kratisi_for_day(daytime):
+    mylist = []
+    query = "SELECT id_kratisis, imera_ora FROM KRATISI"
     cursor.execute(query)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    results = cursor.fetchall()
+    for row in results:
+        if daytime == (row[1])[8:10]:
+            mylist.append(row)
+    counter = len(mylist)
+    conn.close()
+
+    return counter, mylist  # returns number of kratisis + the tuple imera+ora of kratisis for a given day
+
+def get_all_food():
+    query = "SELECT onoma FROM FAGITO"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return [t[0] for t in results]
+
+
+def get_all_drinks():
+    query = "SELECT onoma FROM POTO"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return [t[0] for t in results]
+
+def get_all_trapezia():
+    query = "SELECT id_trapeziou FROM TRAPEZI"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return [t[0] for t in results]
+
+def get_id_from_yliko(name):
+    query = "SELECT onoma, id_ylikoy FROM YLIKA"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for row in results:
+        if row[0] == name:
+            return row[1]
+
+def insert_paraggelia(id_trapeziou, id_ylikoy, id_potoy):
+    cursor.execute('''
+            INSERT INTO PERILAMBANEI (NULL, imer_ora, kostos, id_trapeziou)
+            VALUES (?, ?, ?, ?)
+        ''', id_trapeziou, id_ylikoy, id_potoy)
+    cursor.execute('''
+        INSERT INTO PARAGGELIA (NULL, imer_ora, kostos, id_trapeziou)
+        VALUES (?, ?, ?, ?)
+    ''', id_trapeziou, id_ylikoy, id_potoy)
 
 
 def getdatetime():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-def insert_kritiki(bathmologia, perigrafi, id_pelati):
-    query = "INSERT INTO KRITIKI (id, bathmologia, perigrafi, imerominia, id_pelati) VALUES(NULL,?,?,?,?)"
-    cursor.execute(query, (bathmologia, perigrafi, getdatetime(), id_pelati))
-    conn.commit()
-
-def delete_row_from_pinaka(linenumber, name):
+# de douleuei
+def delete_row_from_pinaka(table_name, id):
     try:
+        cursor.execute(f"DELETE FROM {table_name} WHERE id = ?", (id,))
+    except Exception as e:
+        print("Error " + str(e))
 
+    pass
 
 
 '''
