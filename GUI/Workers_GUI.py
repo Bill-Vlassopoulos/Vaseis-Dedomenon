734a88baf127697f518d8d86a3f01a7cab5673a6
Bill <billyvlassop@gmail.com>
2024-01-13 17:24:04 +0200

    τελειωσε το gui σερβιτορου

--- GUI/Workers_GUI.py
+++ GUI/Workers_GUI.py
@@ -199,11 +199,11 @@ class Paraggelies(QWidget):
             FROM FAGITO
             JOIN PERILAMBANEI ON FAGITO.id_fagitoy = PERILAMBANEI.id_fagitoy
             WHERE id_paraggelias = ?
-            UNION
+            UNION ALL
             SELECT onoma,kostos
             FROM POTO
             JOIN PERILAMBANEI ON POTO.id_potoy = PERILAMBANEI.id_potoy 
-            WHERE id_paraggelias = ?
+            WHERE id_paraggelias = ?;
             """,
             (self.id_paraggelias, self.id_paraggelias),
         )
@@ -240,7 +240,6 @@ class Paraggelies(QWidget):
             results_drink = cursor.fetchall()
 
             if not results_food:
-                print("hello")
                 cursor.execute(
                     """
                 SELECT id_perilambanei FROM PERILAMBANEI WHERE id_paraggelias = ? AND id_potoy = ? LIMIT 1;
@@ -251,7 +250,6 @@ class Paraggelies(QWidget):
                 self.id_perilambanei = results[0]
 
             else:
-                print("hello gay")
                 cursor.execute(
                     """
                 SELECT id_perilambanei FROM PERILAMBANEI WHERE id_paraggelias = ? AND id_fagitoy = ?  LIMIT 1;
@@ -260,7 +258,6 @@ class Paraggelies(QWidget):
                 )
 
                 results = cursor.fetchone()
-                print(results)
                 self.id_perilambanei = results[0]
 
             cursor.execute(
@@ -283,32 +280,36 @@ class Paraggelia_Management(QWidget):
         down_hbox = QHBoxLayout()
         left_vbox = QVBoxLayout()
         right_vbox = QVBoxLayout()
-        fagita = QListWidget()
-        pota = QListWidget()
-        fagita.addItems(["pizza", "souvlaki"])
-        pota.addItems(["νερό", "πορτοκαλάδα", "κόκα κόλα"])
-        trapezi_cmb = QComboBox()
+        self.fagita = QListWidget()
+        self.pota = QListWidget()
+        self.menu_creation()
+        self.trapezi_cmb = QComboBox()
+        self.trapezi_cmb.activated.connect(self.open_paraggelia)
         trapezi_label = QLabel("Eπιλέξτε Τραπέζι:")
         piata_label = QLabel("Επιλέξτε Πιάτα:")
         kostos_label = QLabel("Τελικό Κόστος:")
-        kostos_line_edit = QLineEdit()
-        kostos_line_edit.setFixedSize(80, 40)
+        self.gettables()
+        self.kostos_line_edit = QLineEdit()
+        self.kostos_line_edit.setFixedSize(80, 40)
         pota_label = QLabel("Επιλέξτε Ποτά:")
         add_food_btn = QPushButton("Προσθήκη Πιάτου")
+        add_food_btn.clicked.connect(self.add_food)
         add_drink_btn = QPushButton("Προσθήκη Ποτού")
+        add_drink_btn.clicked.connect(self.add_poto)
         ypovoli_btn = QPushButton("Ανανέωση Παραγγελίας")
         delete_btn = QPushButton("Διαγραφή Προιόντος")
+        delete_btn.clicked.connect(self.delete_proion)
         self.kratiseis_table = QTableWidget()
         self.kratiseis_table.setRowCount(3)
-        self.kratiseis_table.setColumnCount(3)
-        self.kratiseis_table.setHorizontalHeaderLabels(["Όνομα", "Ποσότητα", "Κόστος"])
+        self.kratiseis_table.setColumnCount(2)
+        self.kratiseis_table.setHorizontalHeaderLabels(["Όνομα", "Κόστος"])
 
         left_vbox.addWidget(piata_label)
-        left_vbox.addWidget(fagita)
+        left_vbox.addWidget(self.fagita)
         left_vbox.addStretch()
 
         right_vbox.addWidget(pota_label)
-        right_vbox.addWidget(pota)
+        right_vbox.addWidget(self.pota)
         right_vbox.addStretch()
 
         hbox.addLayout(left_vbox)
@@ -316,20 +317,234 @@ class Paraggelia_Management(QWidget):
         hbox.addLayout(right_vbox)
 
         down_right_vbox.addWidget(trapezi_label)
-        down_right_vbox.addWidget(trapezi_cmb)
+        down_right_vbox.addWidget(self.trapezi_cmb)
         down_right_vbox.addWidget(add_food_btn)
         down_right_vbox.addWidget(add_drink_btn)
         down_right_vbox.addWidget(delete_btn)
         down_right_vbox.addWidget(ypovoli_btn)
         down_right_vbox.addStretch()
         down_right_vbox.addWidget(kostos_label)
-        down_right_vbox.addWidget(kostos_line_edit)
+        down_right_vbox.addWidget(self.kostos_line_edit)
 
         down_hbox.addWidget(self.kratiseis_table)
         down_hbox.addLayout(down_right_vbox)
         main_vbox.addLayout(hbox)
         main_vbox.addLayout(down_hbox)
         self.setLayout(main_vbox)
+
+    def menu_creation(self):
+        query = "select onoma from FAGITO "
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        for row in results:
+            self.fagita.addItem(row[0])
+
+        query = "select onoma from POTO "
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        for row in results:
+            self.pota.addItem(row[0])
+
+    def gettables(self):
+        query = "SELECT id_trapeziou FROM TRAPEZI"
+        cursor.execute(query)
+        results = cursor.fetchall()
+        list = [t[0] for t in results]
+        self.trapezi_cmb.addItems(list)
+
+    def set_table_data(self):
+        self.kratiseis_table.clearContents()
+        self.kratiseis_table.setRowCount(0)
+
+        # Εκτελέστε το κατάλληλο SQL ερώτημα για να πάρετε τα δεδομένα από τη βάση
+        cursor.execute(
+            """
+            SELECT onoma, kostos
+            FROM FAGITO
+            JOIN PERILAMBANEI ON FAGITO.id_fagitoy = PERILAMBANEI.id_fagitoy
+            WHERE id_paraggelias = ?
+            UNION ALL
+            SELECT onoma,kostos
+            FROM POTO
+            JOIN PERILAMBANEI ON POTO.id_potoy = PERILAMBANEI.id_potoy 
+            WHERE id_paraggelias = ?;
+            """,
+            (self.id_paraggelias, self.id_paraggelias),
+        )
+
+        # Πάρτε όλα τα αποτελέσματα
+        results = cursor.fetchall()
+
+        # Ορίστε τον αριθμό των γραμμών του πίνακα
+        self.kratiseis_table.setRowCount(len(results))
+
+        for row_index, row_data in enumerate(results):
+            onoma, kostos = row_data
+
+            # Προσθέστε τα δεδομένα σε κάθε κελί του πίνακα
+            self.kratiseis_table.setItem(row_index, 0, QTableWidgetItem(str(onoma)))
+            self.kratiseis_table.setItem(
+                row_index, 1, QTableWidgetItem(str(kostos) + "0€")
+            )
+        self.calc_cost()
+
+    def open_paraggelia(self):
+        self.id_trapeziou = self.trapezi_cmb.currentText()
+        query = "select id_paraggelias from paraggelia where id_trapeziou=? order by imer_ora DESC limit 1;"
+        cursor.execute(query, (self.id_trapeziou,))
+        results = cursor.fetchone()
+        self.id_paraggelias = results[0]
+        self.set_table_data()
+
+    def add_food(self):
+        self.food_name = self.fagita.currentItem().text()
+        query = "SELECT id_fagitoy FROM FAGITO WHERE onoma='{}'".format(self.food_name)
+        cursor.execute(query)
+        results = cursor.fetchone()
+        self.id_fagitoy = results[0]
+
+        cursor.execute(
+            """
+            INSERT INTO PERILAMBANEI (id_paraggelias, id_fagitoy, id_potoy, id_perilambanei)
+            VALUES (?, ?, NULL, NULL)
+        """,
+            (self.id_paraggelias, self.id_fagitoy),
+        )
+        conn.commit()
+        self.set_table_data()
+
+    def add_poto(self):
+        self.poto_name = self.pota.currentItem().text()
+        query = "SELECT id_potoy FROM POTO WHERE onoma='{}'".format(self.poto_name)
+        cursor.execute(query)
+        results = cursor.fetchall()
+        self.id_potoy = results[0][0]
+
+        cursor.execute(
+            """
+            INSERT INTO PERILAMBANEI (id_paraggelias, id_fagitoy, id_potoy, id_perilambanei)
+            VALUES (?, NULL, ?, NULL)
+        """,
+            (self.id_paraggelias, self.id_potoy),
+        )
+        conn.commit()
+        self.set_table_data()
+
+    def delete_proion(self):
+        selected_row = self.kratiseis_table.currentRow()
+
+        if selected_row >= 0:
+            # Get the name of the item to be deleted
+            item_name = self.kratiseis_table.item(selected_row, 0).text()
+            # Delete the item from the database or perform other necessary actions
+            # ...
+            query = "select id_fagitoy from FAGITO where onoma=?"
+            cursor.execute(query, (item_name,))
+            results_food = cursor.fetchall()
+
+            query = "select id_potoy from POTO where onoma=?"
+            cursor.execute(query, (item_name,))
+            results_drink = cursor.fetchall()
+
+            if not results_food:
+                cursor.execute(
+                    """
+                SELECT id_perilambanei FROM PERILAMBANEI WHERE id_paraggelias = ? AND id_potoy = ? LIMIT 1;
+            """,
+                    (self.id_paraggelias, results_drink[0][0]),
+                )
+                results = cursor.fetchone()
+                self.id_perilambanei = results[0]
+
+            else:
+                cursor.execute(
+                    """
+                SELECT id_perilambanei FROM PERILAMBANEI WHERE id_paraggelias = ? AND id_fagitoy = ?  LIMIT 1;
+            """,
+                    (self.id_paraggelias, results_food[0][0]),
+                )
+
+                results = cursor.fetchone()
+                self.id_perilambanei = results[0]
+
+            cursor.execute(
+                "delete from PERILAMBANEI where id_perilambanei=?",
+                (self.id_perilambanei,),
+            )
+            conn.commit()
+            # Remove the selected row from the QTableWidget
+            self.kratiseis_table.removeRow(selected_row)
+            self.set_table_data()
+
+    def tuple_to_list(self, list):
+        return [item[0] for item in list]
+
+    def get_onoma_from_id_fagitoy(self, id_fagitoy):
+        cursor.execute(
+            """
+            SELECT onoma FROM FAGITO WHERE id_fagitoy = ?
+        """,
+            (id_fagitoy,),
+        )
+        return cursor.fetchone()[0]
+
+    def get_onoma_from_id_potoy(self, id_potoy):
+        cursor.execute(
+            """
+            SELECT onoma FROM POTO WHERE id_potoy = ?
+        """,
+            (id_potoy,),
+        )
+        return cursor.fetchone()[0]
+
+    def calc_cost(self):
+        kostos = 0
+        cursor.execute(
+            """
+            SELECT id_fagitoy from PERILAMBANEI WHERE id_paraggelias = ?
+        """,
+            (self.id_paraggelias,),
+        )
+        food = self.tuple_to_list(cursor.fetchall())
+        food = [item for item in food if item is not None]
+        cursor.execute(
+            """
+                SELECT id_potoy from PERILAMBANEI WHERE id_paraggelias = ?
+            """,
+            (self.id_paraggelias,),
+        )
+        drinks = self.tuple_to_list(cursor.fetchall())
+        drinks = [item for item in drinks if item is not None]
+
+        for item in food:
+            item = self.get_onoma_from_id_fagitoy(item)
+            cursor.execute(
+                """
+                SELECT kostos from FAGITO
+                WHERE onoma = ?
+            """,
+                (item,),
+            )
+            result = cursor.fetchone()
+            if result is not None and result[0] is not None:
+                kostos += float(result[0])
+
+        for item in drinks:
+            item = self.get_onoma_from_id_potoy(item)
+            cursor.execute(
+                """
+                SELECT kostos from POTO
+                WHERE onoma = ?
+            """,
+                (item,),
+            )
+            result = cursor.fetchone()
+            if result is not None and result[0] is not None:
+                kostos += float(result[0])
+        print("Value: " + str(kostos))
+        self.kostos_line_edit.setText(str(kostos) + "€")
 
 
 class Pros_Paradosi(QWidget):
@@ -388,9 +603,11 @@ class Kratiseis(QWidget):
         self.surname = QLineEdit()
         self.tilefono = QLineEdit()
         self.ypovoli_btn = QPushButton("Υποβολή Κράτησης")
+        self.akyrosi_btn = QPushButton("Ακύρωση Κράτησης")
         self.ora_afixis = QComboBox(self)
         self.ora_afixis.addItems(self.gettimes())
         self.ypovoli_btn.clicked.connect(self.ypovoli_kratisis)
+        self.akyrosi_btn.clicked.connect(self.delete_kratisi)
         self.kratiseis_table = QTableWidget()
         self.kratiseis_table.setRowCount(3)
         self.kratiseis_table.setColumnCount(5)
@@ -417,7 +634,7 @@ class Kratiseis(QWidget):
 
         down_right_vbox.addStretch()
         down_right_vbox.addWidget(self.ypovoli_btn)
-        down_right_vbox.addWidget(QPushButton("Ακύρωση Κράτησης"))
+        down_right_vbox.addWidget(self.akyrosi_btn)
         down_right_vbox.addStretch()
 
         down_hbox.addWidget(self.kratiseis_table)
@@ -523,6 +740,32 @@ class Kratiseis(QWidget):
         )
         conn.commit()
         self.get_kratiseis()
+
+    def delete_kratisi(self):
+        selected_row = self.kratiseis_table.currentRow()
+        if selected_row >= 0:
+            # Ανακτήστε τα δεδομένα της επιλεγμένης γραμμής
+            id_trapeziou = self.kratiseis_table.item(selected_row, 0).text()
+            imera_ora = self.kratiseis_table.item(selected_row, 1).text()
+            aritmos_atomon = self.kratiseis_table.item(selected_row, 2).text()
+
+            # Εδώ μπορείτε να προσθέσετε τον κώδικα για τη διαγραφή της επιλεγμένης γραμμής από τη βάση δεδομένων
+            query = "select id_kratisis from kratisi where id_trapeziou='{}' AND imera_ora='{}' AND aritmos_atomon={}".format(
+                id_trapeziou, imera_ora, aritmos_atomon
+            )
+            cursor.execute(query)
+            results = cursor.fetchall()
+            id_kratisis = results[0][0]
+
+            query = "delete from KANEI where id_kratisis={}".format(id_kratisis)
+            cursor.execute(query)
+            conn.commit()
+
+            query = "delete from KRATISI where id_kratisis={}".format(id_kratisis)
+            cursor.execute(query)
+            conn.commit()
+            # Ενημερώστε τον πίνακα εκ νέου
+            self.get_kratiseis()
 
     def gettimes(self):
         return ["19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00"]
